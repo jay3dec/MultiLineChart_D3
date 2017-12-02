e4c501f02f1fb9c83215e4ade79de0690cf13052
Kidhar Bachan <Kidhar.Bachan@absa.co.za>
2017-12-02 17:36:55 +0200

    Datat in, but not displaying

--- format.rb
+++ format.rb
@@ -1,10 +1,12 @@
 
 puts 'var data = [{'
 file = File.open("co2_data.txt")
+
 file.each_line do |line|
-  # line.split.each_slice(1).map{|a|a.join ' '}
-  line.split("\n").map{|a| a.split ' '}
-  puts line[0]
-  # word = line.split(" ")
-  # puts "\"co2: \"#{word}\","
+  word = line.split(' ')
+  puts "\t\"co2: \"#{word[1]}\","
+  puts "\t\"year: \"#{word[0]}\""
+  puts '}, {'
 end
+
+puts '}];'
