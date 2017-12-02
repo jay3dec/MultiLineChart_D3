c2de4f1a718c374992c56dc2a9d07d0b48143c8d
Kidhar Bachan <Kidhar.Bachan@absa.co.za>
2017-12-02 18:30:16 +0200

    Unlabeled axis

--- format.rb
+++ format.rb
@@ -4,8 +4,8 @@ file = File.open("co2_data.txt")
 
 file.each_line do |line|
   word = line.split(' ')
-  puts "\t\"co2: \"#{word[1]}\","
-  puts "\t\"year: \"#{word[0]}\""
+  puts "\t\"co2\": \"#{word[1]}\","
+  puts "\t\"year\": \"#{word[0]}\""
   puts '}, {'
 end
 
