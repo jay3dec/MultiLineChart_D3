f0f00c5f5c4a2764e723a7e4532f78f9f6d47fe7
Kidhar Bachan <kidharb@gmail.com>
2017-12-10 20:12:31 +0200

    Save state

--- json2neato.rb
+++ json2neato.rb
@@ -6,18 +6,16 @@ json = file.read
 
 parsed = JSON.parse(json)
 
-# puts parsed['characters']
 output = File.open( 'graph.dot','w')
-output << 'graph G {'
-output << "\n"
+output << "graph G {\n"
+#output << "splines = true\n"
+output << "fontsize = 8\n"
 parsed['characters'].each do |c|
   if c['faction'] == 'House Stark'
-    puts "#{c['faction']} -- #{c['name']}"
-    output << "House Stark -- #{c['name']}"
+    #puts "#{c['faction']} -- #{c['name']}"
+    output << "\"House Stark\" -- \"#{c['name']}\""
     output << "\n"
   end
 end
-
-output << "\n"
 output << '}'
 output.close
